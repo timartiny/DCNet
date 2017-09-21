382971554f087df5bdcc9bc864b2b4959e357418
Jack Wampler <jack.wampler@colorado.edu>
2017-09-21 00:46:17 -0600

    Redesigned Prng to be stateless per connection.

--- prng/prng.go
+++ prng/prng.go
@@ -7,85 +7,29 @@ import(
     // "fmt"
 )
 
-type Prng struct{
-    cipher  cipher.Block
-    pt      []byte        // Plaintext number to Encrypt for next ct block
-    ct      []byte        // Cipher Text Block
-    used    int           // [0-15] Number of bytes used in Cipher text 
-}
 
-func (p *Prng) Setup(key []byte) error{
+
+func GetBytes( key []byte, n int, nonce []byte) ([]byte, error) {
+
     if len(key) != 16 && len(key) != 24 && len(key) != 32{
-        return errors.New("key incorrect size, must be 16, 24, or 32 bytes")
+        return nil, errors.New("key incorrect size, must be 16, 24, or 32 bytes")
     }
 
-    var err error
+    cipherText := make([]byte, aes.BlockSize+ n )
+    plainText := make([]byte, n)
+
+    for i:=0; i < n; i++{ plainText[i] = 0 }
+
     // initilialize the cipher
-    p.cipher, err = aes.NewCipher(key)
-    p.pt = []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-    // Generate the first Cipher text
-    p.generateBlock()
-
-    return err
-}
-
-func (p *Prng) increment(){
-    carry := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-    p.pt[0] += 1
-    if p.pt[0] == 0 { carry[0] = 1 }
-
-    for i := 1; i <16; i++{
-        if (p.pt[i] + byte(carry[i-1]) == 0) && (carry[i-1] ==1) { carry[i] = 1 }
-        p.pt[i] += byte(carry[i-1])
+    cipherBlock, err := aes.NewCipher(key)
+    if err != nil {
+        return nil, err
     }
 
-    if carry[15] != 0 {
-        // WE HAVE CARRIED OVER THROUGH THE ENTIRE PLAINTEXT
-    }
-}
+    cfbStream := cipher.NewCFBEncrypter(cipherBlock, nonce)
+    cfbStream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+    return cipherText[aes.BlockSize:], nil
 
-func (p *Prng) getByte() byte{
-
-    if p.used == 16 {
-        p.generateBlock()
-    }
-
-    // fmt.Printf("%d\n", p.used)
-    // fmt.Printf("ct - [% x]\n", p.ct)
-    // fmt.Printf("pt - [% x]\n", p.pt)
-
-    b := p.ct[p.used]
-    p.used++
-
-    return b
-}
-
-func (p *Prng) GetBytes(n int) []byte {
-    hold := make([]byte, n)
-
-    // Fill in the message one block of ciphertext at a time.
-    for i := 0; i < n; i++ {
-        hold[i] = p.getByte()
-    }
-
-    return hold
-}
-
-
-func (p *Prng) generateBlock() {
-
-    var ciphertext = make([]byte, aes.BlockSize+len(p.pt))
-    iv := ciphertext[:aes.BlockSize]
-
-    cfb := cipher.NewCFBEncrypter(p.cipher, iv)
-    cfb.XORKeyStream(ciphertext[aes.BlockSize:], p.pt)
-    p.ct = ciphertext[16:]
-
-    // fmt.Printf("New Cipher Text [% x]\n", p.ct)
-
-    p.increment()
-    p.used = 0
 }
 
 
--- prng/prng_test.go
+++ prng/prng_test.go
@@ -5,59 +5,23 @@ import (
     "fmt"
 )
 
-func TestSetup(t *testing.T){
 
-    fmt.Println("** [Test] [Setup]\n")
-    prng := Prng{}
-    var err error
-    k32 := make([]byte, 32)
-
-    err = prng.Setup(k32)
-    t.Log("32 byte key err =", err)
-
-    if len(prng.pt) == 0 {
-        fmt.Println("Setup is not working correctly")
-    }
-
-    fmt.Println("\n** ----------------------------------\n\n")
-}
-
-func TestIncrement(t *testing.T){
-    fmt.Println("** [Test] [Increment]\n")
-
-    prng := Prng{}
-    k32 := make([]byte, 32)
-    prng.Setup(k32)
-
-    for i := 0; i < 10; i++ {
-        fmt.Printf("%d - [% x]\n", i,  prng.pt)
-        prng.increment()
-    }
-
-    for j := 0; j < 250; j++ {
-        prng.increment()
-    }
-    fmt.Printf("260 - [% x]\n", prng.pt)
-
-    for k := 0; k < 65279; k++ {
-        prng.increment()
-    }
-    fmt.Printf("65539 - [% x]\n", prng.pt)
-
-    fmt.Println("\n** ----------------------------------\n\n")
-}
 
 func TestGetBytes_short(t *testing.T){
 
     fmt.Println("** [Test] [GetBytes] - Short\n")
-    prng := Prng{}
+
     k32 := make([]byte, 32)
-    prng.Setup(k32)
+    nonce := make([]byte, 16)
 
 
-    bytes := prng.GetBytes(10)
+    bytes, err := GetBytes(k32, 10, nonce)
+
+    if err != nil {
+        t.Log(err)
+    }
+
     fmt.Printf("[% x]\n", bytes)
-    fmt.Printf("Bytes used - %d\n", prng.used)
 
     fmt.Println("\n** ----------------------------------\n\n")
 }
@@ -66,14 +30,18 @@ func TestGetBytes_short(t *testing.T){
 func TestGetBytes_Long(t *testing.T){
 
     fmt.Println("** [Test] [GetBytes] - Long\n")
-    prng := Prng{}
+
     k32 := make([]byte, 32)
-    prng.Setup(k32)
+    nonce := make([]byte, 16)
 
 
-    bytes := prng.GetBytes(100)
+    bytes, err := GetBytes(k32, 100, nonce)
+
+    if err != nil {
+        t.Log(err)
+    }
+
     fmt.Printf("[% x]\n", bytes)
-    fmt.Printf("Bytes used - %d\n", prng.used)
 
     fmt.Println("\n** ----------------------------------\n\n")
 }
